286c077bfad5cebecddc3670e430fecd95758b8e
Darkelf2024 <174353864+Darkelf2024@users.noreply.github.com>
2026-08-01 00:34:10 -0500

    Update test_darkelf_cli.py

--- tests/test_darkelf_cli.py
+++ tests/test_darkelf_cli.py
@@ -1,17 +1,9 @@
 import hashlib
 from unittest.mock import MagicMock, patch
 
-from importlib.util import find_spec
-
 import pytest
 
-if find_spec("darkelf_cli") is None:
-    pytest.skip(
-        "Darkelf CLI Browser not installed",
-        allow_module_level=True,
-    )
-
-from darkelf_cli import cli
+cli = pytest.importorskip("darkelf_cli.cli")
 
 
 # ----------------------------------------------------------
